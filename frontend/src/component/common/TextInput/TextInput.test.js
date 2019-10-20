d2ecc35b153af1f7e1d37862e7664e1ec0ef355b
Daewoong <dkim94@snu.ac.kr>
2019-10-20 18:05:49 +0900

    feat: add common TextInput component

---
+++ frontend/src/component/common/TextInput/TextInput.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import TextInput from './';
+
+describe('<TextInput />', () => {
+  it('SHOULD match with snapshot', async () => {
+    const { container } = render(
+      <TextInput
+        label="TextInput Label"
+      />
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
