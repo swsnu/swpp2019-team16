00189096da4a0427855cb4c87bf76d0dc86d117f
Myeongseok Seong <mss8087@snu.ac.kr>
2019-10-20 19:50:07 +0900

    feat: add common Box component

--- frontend/src/pages/GroupPage/GroupPage.test.js
+++ frontend/src/pages/GroupPage/GroupPage.test.js
@@ -3,10 +3,8 @@ import GroupPage from './GroupPage';
 import { render, fireEvent } from '@testing-library/react';
 
 describe('<GroupPage />', () => {
-    it('SHOULD match with snapshot', async () => {
-        const { container } = render(
-            <GroupPage />                
-        );
-        expect(container).toMatchSnapshot();
-    });
+  it('SHOULD match with snapshot', async () => {
+    const { container } = render(<GroupPage />);
+    expect(container).toMatchSnapshot();
+  });
 });
--- frontend/src/pages/RequestPage/RequestPage.test.js
+++ frontend/src/pages/RequestPage/RequestPage.test.js
@@ -3,10 +3,8 @@ import RequestPage from './RequestPage';
 import { render, fireEvent } from '@testing-library/react';
 
 describe('<RequestPage />', () => {
-    it('SHOULD match with snapshot', async () => {
-        const { container } = render(
-            <RequestPage />                
-        );
-        expect(container).toMatchSnapshot();
-    });
+  it('SHOULD match with snapshot', async () => {
+    const { container } = render(<RequestPage />);
+    expect(container).toMatchSnapshot();
+  });
 });
