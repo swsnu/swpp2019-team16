04c6204748fba3cd96ee29e13b7e7ea1dc64233c
Daewoong <dkim94@snu.ac.kr>
2019-10-20 18:06:57 +0900

    feat: add common TextInput component

--- frontend/src/component/common/TextInput/TextInput.js
+++ frontend/src/component/common/TextInput/TextInput.js
@@ -3,36 +3,35 @@ import PropTypes from 'prop-types';
 import TextField from '@material-ui/core/TextField';
 
 TextInputMaterial.propTypes = {
-    label: PropTypes.string.isRequired,
-    defaultValue: PropTypes.string,
-    inputProps: PropTypes.object,
-    value: PropTypes.string,
-    name: PropTypes.string,
-    type: PropTypes.string,
-    onChange: PropTypes.func,
+  label: PropTypes.string.isRequired,
+  defaultValue: PropTypes.string,
+  inputProps: PropTypes.object,
+  value: PropTypes.string,
+  name: PropTypes.string,
+  type: PropTypes.string,
+  onChange: PropTypes.func,
 };
 
 function TextInputMaterial({
-    label,
-    defaultValue,
-    inputProps,
-    value,
-    name,
-    type,
-    onChange,
+  label,
+  defaultValue,
+  inputProps,
+  value,
+  name,
+  type,
+  onChange,
 }) {
-    return (
-        <TextField 
-            label={label}
-            defaultValue={defaultValue}
-            inputProps={inputProps}
-            value={value}
-            name={name}
-            type={type}
-            onChange={onChange}
-        />
-    );
+  return (
+    <TextField
+      label={label}
+      defaultValue={defaultValue}
+      inputProps={inputProps}
+      value={value}
+      name={name}
+      type={type}
+      onChange={onChange}
+    />
+  );
 }
 
 export default TextInputMaterial;
-
