f4d972c8d0530dd37d76134dc84281746442abd2
Daewoong <dkim94@snu.ac.kr>
2019-10-20 18:05:49 +0900

    feat: add common TextInput component

---
+++ frontend/src/component/common/TextInput/TextInput.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+
+TextInputMaterial.propTypes = {
+    label: PropTypes.string.isRequired,
+    defaultValue: PropTypes.string,
+    inputProps: PropTypes.object,
+    value: PropTypes.string,
+    name: PropTypes.string,
+    type: PropTypes.string,
+    onChange: PropTypes.func,
+};
+
+function TextInputMaterial({
+    label,
+    defaultValue,
+    inputProps,
+    value,
+    name,
+    type,
+    onChange,
+}) {
+    return (
+        <TextField 
+            label={label}
+            defaultValue={defaultValue}
+            inputProps={inputProps}
+            value={value}
+            name={name}
+            type={type}
+            onChange={onChange}
+        />
+    );
+}
+
+export default TextInputMaterial;
+
