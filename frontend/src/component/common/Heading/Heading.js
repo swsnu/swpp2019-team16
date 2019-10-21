f7eb49abd30bdb5fdafd66e745655cc9a53a943d
Daewoong <dkim94@snu.ac.kr>
2019-10-21 14:40:41 +0900

    feat: add common Heading component

---
+++ frontend/src/component/common/Heading/Heading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Typography from '@material-ui/core/Typography';
+
+HeadingMaterial.propTypes = {
+  variant: PropTypes.string,
+  title: PropTypes.string.isRequired,
+};
+
+function HeadingMaterial({ title }) {
+  return <Typography variant="h1" title={title} />;
+}
+
+export default HeadingMaterial;
