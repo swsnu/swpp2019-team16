7ba641e33f657db2f18e6feb253b9002c02f2c78
Teapot404 <checkmategoo@gmail.com>
2019-10-31 22:57:49 +0900

    feat: add intro page

--- frontend/src/components/intro/Introduction/Introduction.js
+++ frontend/src/components/intro/Introduction/Introduction.js
@@ -17,35 +17,33 @@ Introduction.propTypes = {
   onStart: PropTypes.func.isRequired,
 };
 
-function Introduction({
-	onStart
-}) {
-	return (
-		<div className="Introduction">
-			<Typography variant="h1" gutterBotom paragraph>
-				Welcome to YA-TA!
-			</Typography>
-		
-			<Typography variant="h3" gutterBotom>
-				What is yata?
-			</Typography>
-			
-			<Typography variant="h5" gutterBotom paragraph>
-				Ya-Ta helps students who need to urgently get to class. Within ten minutes, Ya-Ta groups students with these similar, urgent, needs, and grabs a taxi for them.
-			</Typography>
+function Introduction({ onStart }) {
+  return (
+    <div className="Introduction">
+      <Typography variant="h1" gutterBotom paragraph>
+        Welcome to YA-TA!
+      </Typography>
 
-			<div className="startButton">
-				<ButtonMaterial
-				children="Start!"
-				variant="contained"
-				color="primary"
-				onClick={onStart}
-				/>				
-			</div>
-		</div>
-	);
+      <Typography variant="h3" gutterBotom>
+        What is yata?
+      </Typography>
+
+      <Typography variant="h5" gutterBotom paragraph>
+        Ya-Ta helps students who need to urgently get to class. Within ten
+        minutes, Ya-Ta groups students with these similar, urgent, needs, and
+        grabs a taxi for them.
+      </Typography>
+
+      <div className="startButton">
+        <ButtonMaterial
+          children="Start!"
+          variant="contained"
+          color="primary"
+          onClick={onStart}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Introduction;
-
-
