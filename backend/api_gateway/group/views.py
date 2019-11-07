de66a27ff259f9e9be583f0dfd1ae19a3e676a3a
seong <mss8087@snu.ac.kr>
2019-11-07 16:28:37 +0900

    group test

--- backend/api_gateway/group/views.py
+++ backend/api_gateway/group/views.py
@@ -3,11 +3,11 @@ import json
 from django.http \
     import HttpResponseNotAllowed, JsonResponse
 
-from backend.common.command.group_create_command \ 
-    import GroupCreateCommand,  GROUP_CREATE_COMMAND
+from backend.common.command.group_create_command \
+    import GroupCreateCommand, GROUP_CREATE_COMMAND
 from backend.common.command.group_update_command \
     import GroupUpdateCommand, GROUP_UPDATE_COMMAND
-from backend.common.rpc.infra.adapter.redis.redis_rpc_client \ 
+from backend.common.rpc.infra.adapter.redis.redis_rpc_client \
     import RedisRpcClient
 
 """
@@ -27,13 +27,14 @@ def __create_group(request):
     body = json.loads(request.body.decode())
     # TODO: check KeyError
     command = GroupCreateCommand(
-        riders=body['riders'], from_location=body['from_location'], 
+        from_location=body['from_location'], 
         to_location=body['to_location'])
 
     result = RedisRpcClient().call(GROUP_CREATE_COMMAND, command)
+    data = {'jsonrps': result.jsonrpc, 'id':result.id, 'result':result.result}
 
     # TODO: handling exception
-    return with_json_response(status=204, data=result)
+    return with_json_response(status=204, data=data)
 
 def update_group(request):
     if request.method == 'PUT':
@@ -47,6 +48,7 @@ def __update_group(request):
     command = GroupUpdateCommand(driver_id=body['driver_id'])
 
     result = RedisRpcClient().call(GROUP_UPDATE_COMMAND, command)
+    data = {'jsonrps': result.jsonrpc, 'id':result.id, 'result':result.result}
 
     # TODO: handling exception
-    return with_json_response(status=204, data=result)
+    return with_json_response(status=204, data=data)
