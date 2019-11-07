dc76fa6beb1b5c3da227946eee0f01ea2205c005
seong <mss8087@snu.ac.kr>
2019-11-07 15:56:53 +0900

    feat: add group component

--- backend/carpool_request_service/carpool_request/management/commands/listen_command.py
+++ backend/carpool_request_service/carpool_request/management/commands/listen_command.py
@@ -23,23 +23,10 @@ from backend.common.rpc.infra.adapter.redis.redis_rpc_server \
 class Command(BaseCommand):
 
     carpool_request_application_service = CarpoolRequestApplicationService()
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> chore: edit carpool request service
     carpool_request_create_command_handler = CarpoolRequestCreateCommandHandler(
         carpool_request_application_service=carpool_request_application_service)
     carpool_request_delete_command_handler = CarpoolRequestDeleteCommandHandler(
         carpool_request_application_service=carpool_request_application_service)
-<<<<<<< HEAD
-=======
-    user_create_command_handler = UserCreateCommandHandler(
-        user_application_service=user_application_service)
-    user_login_command_handler = UserLoginCommandHandler(
-        user_application_service=user_application_service)
->>>>>>> feat: add carpool_request_service
-=======
->>>>>>> chore: edit carpool request service
     subscriber = RedisMessageSubscriber()
     rpc_server = RedisRpcServer()
 
@@ -50,37 +37,14 @@ class Command(BaseCommand):
                 functools.partial(asyncio.ensure_future,
                                   self.shutdown(signame, loop)))
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-    def login_signal_handler(self, loop):
-        for signame in ('SIGINT', 'SIGTERM'):
-            loop.add_signal_handler(
-                getattr(signal, signame),
-                functools.partial(asyncio.ensure_future,
-                                  self.shutdown(signame, loop)))
-
->>>>>>> feat: add carpool_request_service
-=======
->>>>>>> chore: edit carpool request service
     def handle(self, *args, **options):
         loop = asyncio.get_event_loop()
 
-        self.register_signal_handler(loop)
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        self.login_signal_handler(loop)
->>>>>>> feat: add carpool_request_service
-=======
->>>>>>> chore: edit carpool request service
-        
+        self.register_signal_handler(loop)        
         async def main():
             """
             create subscription tasks
             """
-<<<<<<< HEAD
-<<<<<<< HEAD
             carpool_request_create_subscription_task = asyncio.create_task(
                 self.subscriber.subscribe_message(
                     topic=CARPOOL_REQUEST_CREATE_COMMAND,
@@ -101,11 +65,7 @@ class Command(BaseCommand):
                     topic=CARPOOL_REQUEST_DELETE_COMMAND,
                     request_handler=self.carpool_request_delete_command_handler))
 
-=======
-            user_create_subscription_task = asyncio.create_task(
-=======
             carpool_request_create_subscription_task = asyncio.create_task(
->>>>>>> chore: edit carpool request service
                 self.subscriber.subscribe_message(
                     topic=CARPOOL_REQUEST_CREATE_COMMAND,
                     message_handler=self.carpool_request_create_command_handler))
@@ -119,40 +79,14 @@ class Command(BaseCommand):
                 self.subscriber.subscribe_message(
                     topic=CARPOOL_REQUEST_DELETE_COMMAND,
                     message_handler=self.carpool_request_delete_command_handler))
-
-            carpool_request_delete_rpc_task = asyncio.create_task(
-                self.rpc_server.register_handler(
-<<<<<<< HEAD
-                    topic=USER_LOGIN_COMMAND,
-                    request_handler=self.user_login_command_handler))
-        
->>>>>>> feat: add carpool_request_service
-=======
-                    topic=CARPOOL_REQUEST_DELETE_COMMAND,
-                    request_handler=self.carpool_request_delete_command_handler))
-
->>>>>>> chore: edit carpool request service
             """
             wait until application stop
             """
             await asyncio.gather(
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> chore: edit carpool request service
                 carpool_request_create_subscription_task,
                 carpool_request_create_rpc_task,
                 carpool_request_delete_subscription_task,
                 carpool_request_delete_rpc_task,
-<<<<<<< HEAD
-=======
-                user_create_subscription_task,
-                user_create_rpc_task,
-                user_login_subscription_task,
-                user_login_rpc_task,
->>>>>>> feat: add carpool_request_service
-=======
->>>>>>> chore: edit carpool request service
             )
 
         loop.create_task(main())
