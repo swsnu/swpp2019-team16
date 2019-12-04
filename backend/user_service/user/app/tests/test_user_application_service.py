917a5936f782970863a4de0d21b4b58fca4842ad
JooHyung Kim <14wnrkim@gmail.com>
2019-12-04 11:33:32 +0900

    refactor: user_application_service login (#160)

--- backend/user_service/user/app/tests/test_user_application_service.py
+++ backend/user_service/user/app/tests/test_user_application_service.py
@@ -1,7 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 from backend.user_service.user.app.user_application_service \
     import UserApplicationService
+from backend.user_service.user.domain.rider import Rider
 
 
 class UserApplicationServiceTestCase(TestCase):
@@ -82,11 +84,11 @@ class UserApplicationServiceTestCase(TestCase):
             email=EMAIL, password=PASSWORD,
             user_type=USER_TYPE, car_type='', plate_no='')
         result = self.user_application_service.login(user_id=user1['id'])
-        self.assertEqual(result['status'], 'IDLE')
-        self.assertEqual(result['group'], None)
-        self.assertEqual(result['user']['id'], user1['id'])
-        self.assertEqual(result['user']['email'], EMAIL)
-        self.assertEqual(result['user']['user_type'], USER_TYPE)
+        self.assertEqual(result.status, 'IDLE')
+        self.assertEqual(result.group, None)
+        self.assertEqual(result.user.id, user1['id'])
+        self.assertEqual(result.user.email, EMAIL)
+        self.assertEqual(result.user.user_type, USER_TYPE)
 
         EMAIL = "driver1@gmail.com"
         PASSWORD = 1234
@@ -98,13 +100,13 @@ class UserApplicationServiceTestCase(TestCase):
             email=EMAIL, password=PASSWORD,
             user_type=USER_TYPE, car_type=CAR_TYPE, plate_no=PLATE_NO)
         result = self.user_application_service.login(user_id=user2['id'])
-        self.assertEqual(result['status'], 'IDLE')
-        self.assertEqual(result['group'], None)
-        self.assertEqual(result['user']['id'], user2['id'])
-        self.assertEqual(result['user']['email'], EMAIL)
-        self.assertEqual(result['user']['user_type'], USER_TYPE)
-        self.assertEqual(result['user']['vehicle']['car_type'], CAR_TYPE)
-        self.assertEqual(result['user']['vehicle']['plate_no'], PLATE_NO)
+        self.assertEqual(result.status, 'IDLE')
+        self.assertEqual(result.group, None)
+        self.assertEqual(result.user.id, user2['id'])
+        self.assertEqual(result.user.email, EMAIL)
+        self.assertEqual(result.user.user_type, USER_TYPE)
+        self.assertEqual(result.user.vehicle.car_type, CAR_TYPE)
+        self.assertEqual(result.user.vehicle.plate_no, PLATE_NO)
 
     def test_logout(self):
         EMAIL = 'test@gmail.com'
@@ -119,3 +121,48 @@ class UserApplicationServiceTestCase(TestCase):
 
         self.user_application_service.login(user_id=user['id'])
         self.user_application_service.logout(user_id=user['id'])
+
+    def test_create_rider_if_not_exist_when_rider_exist(self):
+        user = get_user_model().objects.create_user(
+            id=123,
+            email='zeroFruit@gmail.com',
+            password='password',
+            user_type='rider'
+        )
+        rider = Rider.objects.create(user_id=user.id, status="IDLE")
+
+        result = self.user_application_service._create_rider_if_not_exist(user.id)
+        self.assertEqual(result.user.id, user.id)
+        self.assertEqual(result.id, rider.id)
+
+    def test_create_rider_or_driver_if_not_exist_when_user_type_is_rider(self):
+        user = get_user_model().objects.create_user(
+            id=123,
+            email='zeroFruit@gmail.com',
+            password='password',
+            user_type='rider'
+        )
+        result = self.user_application_service._create_rider_or_driver_if_not_exist(
+            user_type='rider', user_id=user.id
+        )
+
+        self.assertEqual(result.user.id, user.id)
+
+    def test_create_rider_or_driver_if_not_exist_when_user_type_is_driver(self):
+        user = get_user_model().objects.create_user(
+            id=123,
+            email='zeroFruit@gmail.com',
+            password='password',
+            user_type='driver'
+        )
+        result = self.user_application_service._create_rider_or_driver_if_not_exist(
+            user_type='driver', user_id=user.id
+        )
+
+        self.assertEqual(result.user.id, user.id)
+
+    def test_create_rider_or_driver_if_not_exist_return_value_error(self):
+        result = self.user_application_service._create_rider_or_driver_if_not_exist(
+            user_type='something_wrong', user_id=1
+        )
+        self.assertTrue(type(result) is ValueError)
