05937237bf865da417b9a75707e3bcb0d42e80ec
seong <mss8087@snu.ac.kr>
2019-12-14 16:39:51 +0900

    feat: driver final detail css

--- frontend/src/containers/DriverDetailContainer/DriverDetailContainer.js
+++ frontend/src/containers/DriverDetailContainer/DriverDetailContainer.js
@@ -24,7 +24,7 @@ function DriverDetailContainer({ history }) {
     },
     [dispatch, user, history],
   );
-
+/*
   if (!user) {
     return <div>we are loading user...</div>;
   }
@@ -36,6 +36,13 @@ function DriverDetailContainer({ history }) {
   return (
     <DriverDetail user={user} group={group} onClickConfirm={onClickConfirm} />
   );
+  */
+ return(
+   <DriverDetail
+    user={{id:1}}
+    group={{groupId:1}}
+   />
+ );
 }
 
 export default withRouter(DriverDetailContainer);
